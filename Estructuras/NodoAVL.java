2dbc74b7200383da7dbfe3bd5b18b77d0c52d64e
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-24 17:16:56 -0500

    Fix constructor formatting in NodoAVL class

--- Estructuras/NodoAVL.java
+++ Estructuras/NodoAVL.java
@@ -1,13 +1,11 @@
 package estructuras;
 
 public class NodoAVL<T extends Comparable<T>> {
-
     T dato;
     NodoAVL<T> izquierdo;
     NodoAVL<T> derecho;
     int altura;
-    
- public NodoAVL(T dato) {
+public NodoAVL(T dato) {
         this.dato = dato;
         this.izquierdo = null;
         this.derecho = null;
