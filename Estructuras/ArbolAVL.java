1d820f5d6b19c67f7d812b850170ad4aaf3e23db
JazminCarrizalesArenas <jazmin.carrizales.arenas@gmail.com>
2026-06-24 15:32:36 -0500

    Update ArbolAVL.java

--- Estructuras/ArbolAVL.java
+++ Estructuras/ArbolAVL.java
@@ -7,3 +7,19 @@ public class ArbolAVL<T extends Comparable<T>> {
     public ArbolAVL() {
         this.raiz = null;
     }
+// Altura y balance
+private int obtenerAltura(NodoAVL<T> nodo) {
+        if (nodo == null) return 0;
+        return nodo.altura;
+    }
+
+    private int obtenerBalance(NodoAVL<T> nodo) {
+        if (nodo == null) return 0;
+        return obtenerAltura(nodo.izquierdo) - obtenerAltura(nodo.derecho);
+    }
+
+    private void actualizarAltura(NodoAVL<T> nodo) {
+        nodo.altura = 1 + Math.max(obtenerAltura(nodo.izquierdo),
+                                   obtenerAltura(nodo.derecho));
+    }
+//Rotaciones
