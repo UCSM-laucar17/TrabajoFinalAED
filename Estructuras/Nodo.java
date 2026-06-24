e8034b3b46736bacd6f9355e0cf1e9924ceb6b9f
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-24 17:21:04 -0500

    Fix formatting and add newline at end of Nodo.java

--- Estructuras/Nodo.java
+++ Estructuras/Nodo.java
@@ -1,27 +1,21 @@
 package Estructuras;
 
 public class Nodo<E> {
-
     E data;
     Nodo<E> next;
-
     public Nodo(E data) {
         this.data = data;
         this.next = null;
     }
-
     public E getData() {
         return data;
     }
-
     public void setData(E data) {
         this.data = data;
     }
-
     public Nodo<E> getNext() {
         return next;
     }
-
     public void setNext(Nodo<E> next) {
         this.next = next;
     }
