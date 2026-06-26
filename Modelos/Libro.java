c2746ed5d4cae8722da6ef09b09d0c135e310029
DiagoDavalos <diagoeduardo006@gmail.com>
2026-06-25 22:44:50 -0500

    Add Libro class with attributes and constructor
    crenado la clase libro  :)

--- Modelos/Libro.java
+++ Modelos/Libro.java
@@ -1,1 +1,21 @@
+package modelos;
+public class Libro implements Comparable<Libro> {
 
+    // Atributos
+    private int codigo;
+    private String titulo;
+    private String autor;
+    private String categoria;
+    private int anio;
+    private boolean estado; // true = Disponible | false = Prestado
+
+    // Constructor 
+    public Libro(int codigo, String titulo, String autor,
+                 String categoria, int anio, boolean estado) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+        this.anio = anio;
+        this.estado = estado;
+    }
