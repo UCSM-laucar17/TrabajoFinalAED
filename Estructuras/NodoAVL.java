ec25d258fa2130004b5f7db2ea4e9888315e6005
DiagoDavalos <diagoeduardo006@gmail.com>
2026-06-26 01:20:17 -0500

    Refactor NodoAVL class to include getter methods
    Added getter methods for 'izquierdo', 'derecho', and 'altura'.

--- Estructuras/NodoAVL.java
+++ Estructuras/NodoAVL.java
@@ -1,25 +1,28 @@
 package estructuras;
 
-public class NodoAVL<T extends Comparable<T>> {
-    T dato;
-    NodoAVL<T> izquierdo;
-    NodoAVL<T> derecho;
-    int altura;
-public NodoAVL(T dato) {
-        this.dato = dato;
-        this.izquierdo = null;
-        this.derecho = null;
-        this.altura = 1;
+    public class NodoAVL<T extends Comparable<T>> {
+        T dato;
+        NodoAVL<T> izquierdo;
+        NodoAVL<T> derecho;
+        int altura;
+    public NodoAVL(T dato) {
+            this.dato = dato;
+            this.izquierdo = null;
+            this.derecho = null;
+            this.altura = 1;
+        }
+    public T getDato() {
+        return dato;
     }
-public T getDato() {
-    return dato;
-}
-
-public NodoAVL<T> getIzquierdo() {
-    return izquierdo;
-}
-
-public NodoAVL<T> getDerecho() {
-    return derecho;
-}
+    
+    public NodoAVL<T> getIzquierdo() {
+        return izquierdo;
+    }
+    
+    public NodoAVL<T> getDerecho() {
+        return derecho;
+    }
+     public int getAltura() {
+        return altura;
+    }
 }
