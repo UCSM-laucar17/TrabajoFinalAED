dc0f125178021c354ad84c711612de83c4c48d0d
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-26 21:00:37 -0500

    Update Libro.java

--- Modelos/Libro.java
+++ Modelos/Libro.java
@@ -8,7 +8,6 @@ public class Libro implements Comparable<Libro> {
     private String categoria;
     private int anio;
     private boolean estado; // true = Disponible | false = Prestado
-
     // Constructor 
     public Libro(int codigo, String titulo, String autor,
                  String categoria, int anio, boolean estado) {
@@ -20,62 +19,46 @@ public class Libro implements Comparable<Libro> {
         this.estado = estado;
     }
      // Getters
-
     public int getCodigo() {
         return codigo;
     }
-
     public String getTitulo() {
         return titulo;
     }
-
     public String getAutor() {
         return autor;
     }
-
     public String getCategoria() {
         return categoria;
     }
-
     public int getAnio() {
         return anio;
     }
-
     public boolean getEstado() {
         return estado;
     }
-    
      // Setters
-
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
     public void setAutor(String autor) {
         this.autor = autor;
     }
-
     public void setCategoria(String categoria) {
         this.categoria = categoria;
     }
-
     public void setAnio(int anio) {
         this.anio = anio;
     }
-
     public void setEstado(boolean estado) {
         this.estado = estado;
     }
      // Comparable
-
-
     @Override
     public int compareTo(Libro otro) {
-
         if (this.codigo < otro.codigo){
             return -1;
         }
@@ -83,22 +66,16 @@ public class Libro implements Comparable<Libro> {
             return 1;
         }
         return 0;
-    }
-
+    } 
     // toString()
-  
-
     @Override
     public String toString() {
-
         String estadoLibro;
-
         if (estado) {
         estadoLibro = "Disponible";
         } else {
         estadoLibro = "Prestado";
         }
-
         return "Código: " + codigo +
                " | Título: " + titulo +
                " | Autor: " + autor +
@@ -106,5 +83,4 @@ public class Libro implements Comparable<Libro> {
                " | Año: " + anio +
                " | Estado: " + estadoLibro;
     }
-
 }
