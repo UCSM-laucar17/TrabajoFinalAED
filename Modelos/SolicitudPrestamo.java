4e624547a07aad5219b1e148d87ad0e0e60755c1
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-28 14:28:55 -0500

    Update SolicitudPrestamo.java

--- Modelos/SolicitudPrestamo.java
+++ Modelos/SolicitudPrestamo.java
@@ -2,16 +2,13 @@
 import java.time.LocalDate;
 
 public class SolicitudPrestamo {
-
     private String codigoEstudiante;
     private String nombreEstudiante;
     private String codigoLibro;
     private LocalDate fechaSolicitud;
-
-    public SolicitudPrestamo(String codigoEstudiante, String nombreEstudiante, String codigoLibro) {
+    public SolicitudPrestamo(String código Estudiante, String nombreEstudiante, String codigoLibro) {
         if (codigoEstudiante == null) {
             throw new IllegalArgumentException("El código del estudiante no puede estar vacío.");
-
         }
         if (nombreEstudiante == null) {
             throw new IllegalArgumentException("El nombre del estudiante no puede estar vacío.");
@@ -19,16 +16,14 @@ public class SolicitudPrestamo {
         if (codigoLibro == null) {
             throw new IllegalArgumentException("El código del libro no puede estar vacío.");
         }
-
         this.codigoEstudiante = codigoEstudiante;
         this.nombreEstudiante = nombreEstudiante;
         this.codigoLibro      = codigoLibro;
         this.fechaSolicitud   = LocalDate.now();
     }
-
-    public String getCodigoEstudiante() { return codigoEstudiante; }
+    public String getCodigoEstudiante(){ return codigoEstudiante; }
     public String getNombreEstudiante() { return nombreEstudiante; }
-    public String getCodigoLibro()      { return codigoLibro; }
+    public String getCodigoLibro() { return codigoLibro; }
     public LocalDate getFechaSolicitud(){ return fechaSolicitud; }
     @Override
     public String toString() {
