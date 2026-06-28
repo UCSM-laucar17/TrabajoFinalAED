b898fd5918e68d5facb47535285ef1cde41d8193
Alexandra Arce <alexandra.arceq@estudiante.ucsm.edu.pe>
2026-06-28 10:07:24 -0500

    Update Prestamo.java

--- Modelos/Prestamo.java
+++ Modelos/Prestamo.java
@@ -1,1 +1,49 @@
+import java.time.LocalDate;
 
+public class Prestamo {
+
+    private String codigoEstudiante;
+    private String nombreEstudiante;
+    private String codigoLibro;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(SolicitudPrestamo solicitud) {
+        this.codigoEstudiante = solicitud.getCodigoEstudiante();
+        this.nombreEstudiante = solicitud.getNombreEstudiante();
+        this.codigoLibro      = solicitud.getCodigoLibro();
+        this.fechaPrestamo    = LocalDate.now();
+        this.fechaDevolucion  = null;
+    }
+
+    public void registrarDevolucion() {
+        this.fechaDevolucion = LocalDate.now();
+    }
+
+    public boolean estaActivo() {
+        return fechaDevolucion == null;
+    }
+
+    public String getCodigoEstudiante(){ 
+        return codigoEstudiante; 
+    }
+    public String getNombreEstudiante() { 
+        return nombreEstudiante; 
+    }
+    public String getCodigoLibro(){ 
+        return codigoLibro; 
+    }
+    public LocalDate getFechaPrestamo() { 
+        return fechaPrestamo; 
+    }
+    public LocalDate getFechaDevolucion(){ 
+        return fechaDevolucion; 
+    }
+
+    @Override
+    public String toString() {
+        String dev = (fechaDevolucion != null) ? fechaDevolucion.toString() : "En préstamo";
+        return String.format("[Préstamo] Libro: %s | Estudiante: %s | Prestado: %s | Devuelto: %s",
+                codigoLibro, nombreEstudiante, fechaPrestamo, dev);
+    }
+}
