78a747983934f3e38e0ff1a09cfde802e66665df
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 04:21:34 -0500

    Implement AVLTest for Biblioteca functionality
    Added a test class for Biblioteca to register, search, and delete books.

--- Test/AVLTest.java
+++ Test/AVLTest.java
@@ -1,1 +1,23 @@
+package Test;
 
+import Modelos.Libro;
+import Servicios.Biblioteca;
+
+public class AVLTest {
+
+    public static void main(String[] args) {
+        Biblioteca biblioteca = new Biblioteca();
+        biblioteca.registrarLibro(new Libro(10,"Java","Autor A","Programación",2020,true));
+        biblioteca.registrarLibro(new Libro(5,"C++","Autor B","Programación",2018,true));
+
+
+        System.out.println("catalogo");
+        biblioteca.mostrarLibros();
+        System.out.println("\nBuscar:");
+        System.out.println(biblioteca.buscarCodigo(5));
+        System.out.println("\nEliminar");
+        biblioteca.eliminarLibro(10);
+        System.out.println("\ncatalogo:");
+        biblioteca.mostrarLibros();
+    }
+}
