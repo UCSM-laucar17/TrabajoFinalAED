b7f284f1d25fed9849e425fb980dd482dfa9a499
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 09:50:00 -0500

    Refactor ColaTest.java to improve formatting

--- Test/ColaTest.java
+++ Test/ColaTest.java
@@ -1,30 +1,21 @@
-
 package Test;
-
 import Estructuras.ColaGenerica;
+
 public class ColaTest {
     public static void main(String[] args) {
-
         ColaGenerica<String> cola = new ColaGenerica<>();
-
         cola.enqueue("Lola");
         cola.enqueue("Paluza");
         cola.enqueue("exe404");
-
-
         System.out.println("Contenido de la cola:");
         cola.mostrar();
-
         try{
             System.out.println("\nFrente: " + cola.peek());
             System.out.println("\nSale: " + cola.dequeue());
             System.out.println("\nNueva cola:");
             cola.mostrar();
-
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
-
     }
-
 }
