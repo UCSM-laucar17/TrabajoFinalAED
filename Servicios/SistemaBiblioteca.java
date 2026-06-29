7d551174721b5ea051b68454fd42a3be755a6780
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 03:50:54 -0500

    Initialize GestorPrestamos with Biblioteca instance

--- Servicios/SistemaBiblioteca.java
+++ Servicios/SistemaBiblioteca.java
@@ -3,5 +3,6 @@ package Servicios;
 public class SistemaBiblioteca {
 
     public static Biblioteca biblioteca = new Biblioteca();
+    public static GestorPrestamos gestorPrestamos =new GestorPrestamos(biblioteca);
 
 }
