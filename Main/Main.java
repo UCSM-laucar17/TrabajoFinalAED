309691c3a9d24adce52e230cf1a8ebc2dfaa8a71
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 09:42:56 -0500

    Refactor Main.java by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- Main/Main.java
+++ Main/Main.java
@@ -1,11 +1,8 @@
-
-
 package Main;
 
 import Interfaz.VentanaPrincipal;
 import Servicios.GestorArchivos;
 import Servicios.SistemaBiblioteca;
-
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 import java.awt.event.WindowAdapter;
@@ -15,24 +12,18 @@ public class Main {
     public static void main(String[] args) {
         // Cargar libros almacenados
         GestorArchivos.cargarLibrosCSV(SistemaBiblioteca.biblioteca);
-
         javax.swing.SwingUtilities.invokeLater(() -> {
             VentanaPrincipal ventana = new VentanaPrincipal();
-
             // Guardar automáticamente al cerrar
             ventana.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
             ventana.addWindowListener(new WindowAdapter() {
                 public void windowClosing(WindowEvent e) {
-
                     GestorArchivos.guardarLibrosCSV(SistemaBiblioteca.biblioteca);
                     ventana.dispose();
                     System.exit(0);
                 }
             });
             ventana.setVisible(true);
-
         });
-
     }
-
 }
