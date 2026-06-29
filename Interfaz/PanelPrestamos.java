6af9fa090702c255977696ad96e9b7657e2f1984
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 01:54:05 -0500

    Add PanelPrestamos class with UI components

--- Interfaz/PanelPrestamos.java
+++ Interfaz/PanelPrestamos.java
@@ -1,1 +1,23 @@
+package Interfaz;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+public class PanelPrestamos extends JPanel {
+
+    private JTextField txtCodigoEstudiante;
+    private JTextField txtNombre;
+    private JTextField txtCodigoLibro;
+
+    private JButton btnRegistrar;
+    private JButton btnConsultar;
+    private JButton btnAtender;
+    private JButton btnDevolver;
+    private JButton btnLimpiar;
+
+    private JTextArea areaHistorial;
+
+    private JScrollPane scroll;
+
+}
