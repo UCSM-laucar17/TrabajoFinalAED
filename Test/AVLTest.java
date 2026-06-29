6f9b84e1565feb26f480885e278d0a6d474d735a
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 09:49:35 -0500

    Refactor AVLTest.java for cleaner output

--- Test/AVLTest.java
+++ Test/AVLTest.java
@@ -1,16 +1,12 @@
 package Test;
-
 import Modelos.Libro;
 import Servicios.Biblioteca;
 
 public class AVLTest {
-
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
         biblioteca.registrarLibro(new Libro(10,"Java","Autor A","Programación",2020,true));
         biblioteca.registrarLibro(new Libro(5,"C++","Autor B","Programación",2018,true));
-
-
         System.out.println("catalogo");
         biblioteca.mostrarLibros();
         System.out.println("\nBuscar:");
