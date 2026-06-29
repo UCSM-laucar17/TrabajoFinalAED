e8188c9a58adfa9b030b73934e6bf8de7d5940f3
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 04:09:57 -0500

    Add CSV export functionality to ExportadorReportes

--- Reportes/ExportadorReportes.java
+++ Reportes/ExportadorReportes.java
@@ -1,1 +1,17 @@
 
+
+package Reportes;
+
+import java.io.*;
+
+public class ExportadorReportes {
+  public static void exportarCSV(String contenido){
+        try{
+            BufferedWriter bw=new BufferedWriter(new FileWriter("ReporteBiblioteca.csv"));
+            bw.write(contenido);
+            bw.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
