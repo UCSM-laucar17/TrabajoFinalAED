404f8fd993e5e5f4c57f289d680103b09dd9c8d6
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 04:06:43 -0500

    Prevent window closure on exit
    Set default close operation to prevent window closure without prompt.

--- Main/Main.java
+++ Main/Main.java
@@ -1,1 +1,30 @@
 
+
+package Main;
+
+import Interfaz.VentanaPrincipal;
+import Servicios.GestorArchivos;
+import Servicios.SistemaBiblioteca;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class Main {
+    public static void main(String[] args) {
+        // Cargar libros almacenados
+        GestorArchivos.cargarLibrosCSV(SistemaBiblioteca.biblioteca);
+
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            VentanaPrincipal ventana = new VentanaPrincipal();
+
+            // Guardar automáticamente al cerrar
+            ventana.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+            
+
+        });
+
+    }
+
+}
