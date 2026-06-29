431bcaaf4da1751486f9768952c477814c6967f8
Laura <81392895+lau-carbajal17@users.noreply.github.com>
2026-06-29 03:15:14 -0500

    Add Colores class for color constants

---
+++ Interfaz/Colores.java
@@ -0,0 +1,13 @@
+package Interfaz;
+
+import java.awt.Color;
+//interfaz Diseño de paneles
+public class Colores {
+    public static final Color FONDO = new Color(236,240,241);
+    public static final Color MENU = new Color(44,62,80);
+    public static final Color BOTON = new Color(52,152,219);
+    public static final Color BOTON_HOVER = new Color(41,128,185);
+    public static final Color PANEL = Color.WHITE;
+    public static final Color TEXTO = Color.WHITE;
+
+}
