1692808feb961ef146beead888bda3be3875f224
githudbegazo <shanellabegazo@gmail.com>
2026-06-30 07:08:05 -0500

    Update GestorArchivos.java

--- Servicios/GestorArchivos.java
+++ Servicios/GestorArchivos.java
@@ -8,11 +8,12 @@ public class GestorArchivos {
     private static final String RUTA = "libros.csv";
     public static void guardarLibrosCSV(Biblioteca biblioteca) {
         try {
+            //creamos un bufferWriter con la RUTA de nuestro parametro
             BufferedWriter bw = new BufferedWriter(new FileWriter(RUTA));
             bw.write("codigo,titulo,autor,categoria,anio,estado");
-            bw.newLine();
-            ListaEnlazada<Libro> lista = biblioteca.obtenerTodosLosLibros();
-            for(int i=0;i<lista.size();i++){
+            bw.newLine(); //saltamos linea
+            ListaEnlazada<Libro> lista = biblioteca.obtenerTodosLosLibros(); //obtenemos y almacenamos la lista de libros
+            for(int i=0;i<lista.size();i++){ //recorremos todos los datos de lista y masrcamos si estado disponible o prestando 
                 Libro l=lista.obtener(i);
                 String estado;
                 if(l.getEstado())
@@ -25,23 +26,24 @@ public class GestorArchivos {
             bw.close();
             System.out.println("CSV guardado.");
         }catch(IOException e){
-            System.out.println(e.getMessage());
+            System.out.println(e.getMessage()); //mostramosmensaje si ocurre una excepcion
         }
     }
-    public static void cargarLibrosCSV(Biblioteca biblioteca){
+    public static void cargarLibrosCSV(Biblioteca biblioteca){  //cargamos los libros den archivo
         try{
             BufferedReader br=new BufferedReader(new FileReader(RUTA));
             String linea;
             br.readLine();
-            while((linea=br.readLine())!=null){
-                String datos[]=linea.split(",");
+            //
+            while((linea=br.readLine())!=null){ 
+                String datos[]=linea.split(","); //separa partes del archivo por comas
                 int codigo=Integer.parseInt(datos[0]);
-                String titulo=datos[1];
+                String titulo=datos[1]; //convertimos el titulo en string y asi los siguiente
                 String autor=datos[2];
                 String categoria=datos[3];
                 int anio=Integer.parseInt(datos[4]);
                 boolean estado=datos[5].equalsIgnoreCase("Disponible");
-                biblioteca.registrarLibro(new Libro(codigo,titulo,autor,categoria,anio,estado));
+                biblioteca.registrarLibro(new Libro(codigo,titulo,autor,categoria,anio,estado)); //creamosu n nuevi ibjeto y lo agregamos a la biblioteca
             }
             br.close();
         }catch(Exception e){
